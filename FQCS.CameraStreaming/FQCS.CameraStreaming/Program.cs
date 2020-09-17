a99c67545052b133de9ceb8a7a343d31c74b1703
trannamtrung1st <trannamtrung1st@gmail.com>
2020-09-17 16:17:31 +0700

    Add camera streaming

---
+++ FQCS.CameraStreaming/FQCS.CameraStreaming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FQCS.CameraStreaming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
