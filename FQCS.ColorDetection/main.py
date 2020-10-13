1c3010571ebef0c9da376050ee31ab6261e8b482
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-13 10:35:13 +0700

    reset

--- FQCS.ColorDetection/main.py
+++ FQCS.ColorDetection/main.py
@@ -1,13 +1,14 @@
 from FQCS.detector import FQCSDetector
 import json
 
+
 def main():
     # init
     detector = FQCSDetector()
-    
+
     # save config
     detector.save_config("config.json")
-    
+
     # load config
     detector.load_config("config.json")
     c_dict = detector.config.get_dict()
