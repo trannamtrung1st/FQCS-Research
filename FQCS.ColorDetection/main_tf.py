7320199c82b7722c216ec5878111b2839c680d76
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-13 19:20:42 +0700

    add main tf

--- FQCS.ColorDetection/main_tf.py
+++ FQCS.ColorDetection/main_tf.py
@@ -7,69 +7,70 @@ import cv2
 import numpy as np
 import os
 
-if not os.path.exists("./yolov4.h5"):
-    convert_darknet_weights("./yolov4-custom_best.weights",
-                            "./yolov4.h5", (416, 416, 3),
-                            1,
-                            weights=None)
-    return
 
-HEIGHT, WIDTH = (640, 320)
-model = YOLOv4(input_shape=(HEIGHT, WIDTH, 3),
-               anchors=YOLOV4_ANCHORS,
-               num_classes=1,
-               training=False,
-               yolo_max_boxes=100,
-               yolo_iou_threshold=0.5,
-               yolo_score_threshold=0.5,
-               weights=None)
+def main():
+    if not os.path.exists("./yolov4.h5"):
+        convert_darknet_weights("./yolov4-custom_best.weights",
+                                "./yolov4.h5", (416, 416, 3),
+                                1,
+                                weights=None)
+        return
 
-model.load_weights("./yolov4.h5")
-model.summary()
+    HEIGHT, WIDTH = (640, 320)
+    model = YOLOv4(input_shape=(HEIGHT, WIDTH, 3),
+                   anchors=YOLOV4_ANCHORS,
+                   num_classes=1,
+                   training=False,
+                   yolo_max_boxes=100,
+                   yolo_iou_threshold=0.5,
+                   yolo_score_threshold=0.5,
+                   weights=None)
 
-# COCO classes
-CLASSES = ['dirt']
+    model.load_weights("./yolov4.h5")
+    model.summary()
 
-# colors for visualization
-COLORS = [[0.000, 0.447, 0.741]]
+    # COCO classes
+    CLASSES = ['dirt']
 
-import matplotlib.pyplot as plt
+    import matplotlib.pyplot as plt
+
+    def plot_results(pil_img, boxes, scores, classes):
+        plt.imshow(pil_img)
+        ax = plt.gca()
+
+        for (xmin, ymin, xmax, ymax), score, cl in zip(boxes.tolist(),
+                                                       scores.tolist(),
+                                                       classes.tolist()):
+            if score > 0.5:
+                ax.add_patch(
+                    plt.Rectangle((xmin, ymin),
+                                  xmax - xmin,
+                                  ymax - ymin,
+                                  fill=False,
+                                  color='r',
+                                  linewidth=2))
+                text = f'{CLASSES[cl]}: {score:0.2f}'
+                ax.text(xmin, ymin, text, fontsize=10)
+        plt.axis('off')
+        plt.show()
+
+    while True:
+        img = cv2.imread("FQCS_detector/data/1/dirty_sorted/" + str(np.random.randint(151, 324)) +
+                         ".jpg")
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        img = cv2.resize(img, (WIDTH, HEIGHT))
+        images = np.array([img]) / 255.
+        boxes, scores, classes, valid_detections = model.predict(images)
+
+        plot_results(
+            images[0],
+            boxes[0] * [WIDTH, HEIGHT, WIDTH, HEIGHT],
+            scores[0],
+            classes[0].astype(int),
+        )
+        if (cv2.waitKey() == ord('e')):
+            break
 
 
-def plot_results(pil_img, boxes, scores, classes):
-    plt.imshow(pil_img)
-    ax = plt.gca()
-
-    for (xmin, ymin, xmax, ymax), score, cl in zip(boxes.tolist(),
-                                                   scores.tolist(),
-                                                   classes.tolist()):
-        if score > 0.5:
-            ax.add_patch(
-                plt.Rectangle((xmin, ymin),
-                              xmax - xmin,
-                              ymax - ymin,
-                              fill=False,
-                              color=COLORS[cl % 6],
-                              linewidth=2))
-            text = f'{CLASSES[cl]}: {score:0.2f}'
-            ax.text(xmin, ymin, text, fontsize=10)
-    plt.axis('off')
-    plt.show()
-
-
-while True:
-    img = cv2.imread("dirty_sorted/" + str(np.random.randint(151, 324)) +
-                     ".jpg")
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (WIDTH, HEIGHT))
-    images = np.array([img]) / 255.
-    boxes, scores, classes, valid_detections = model.predict(images)
-
-    plot_results(
-        images[0],
-        boxes[0] * [WIDTH, HEIGHT, WIDTH, HEIGHT],
-        scores[0],
-        classes[0].astype(int),
-    )
-    if (cv2.waitKey() == ord('e')):
-        break
+if __name__ == "__main__":
+    main()
