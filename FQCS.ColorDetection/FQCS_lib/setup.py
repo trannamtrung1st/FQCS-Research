f16284e685683c3bf568b014947905ecfcbc2711
trannamtrung1st <trannamtrung1st@gmail.com>
2020-10-30 15:15:19 +0700

    update ver

--- FQCS.ColorDetection/FQCS_lib/setup.py
+++ FQCS.ColorDetection/FQCS_lib/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="FQCS",
-    version="1.0.6",
+    version="1.0.7",
     author="FQCS Team",
     author_email="trannamtrung1st@gmail.com",
     description="This is for FQCS Capstone Project",
